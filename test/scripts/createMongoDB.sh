f076c08fc81d013322e1fef12f6ecb2186da1c66
Maria Arias de Reyna <ariasdereyna@redhat.com>
2020-06-19 13:35:41 +0200

    sldfkjs

--- test/scripts/createMongoDB.sh
+++ test/scripts/createMongoDB.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-oc get templates --all-namespaces
+echo "Templates"
+echo $(oc get templates --all-namespaces)
+echo "End Templates"
 
 oc new-app -n ${YAKS_NAMESPACE} --name="mongodb" --template=mongodb-persistent  \
 -e MONGODB_USER=camel-k-example -e MONGODB_PASSWORD=transformations  \
