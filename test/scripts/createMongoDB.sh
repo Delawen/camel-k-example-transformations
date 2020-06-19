c25e8f29445f34480baf9241933aaaa447d0573a
Maria Arias de Reyna <ariasdereyna@redhat.com>
2020-06-19 13:24:45 +0200

    Just to check playgroud

--- test/scripts/createMongoDB.sh
+++ test/scripts/createMongoDB.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+oc get templates --all-namespaces
+
 oc new-app -n ${YAKS_NAMESPACE} --name="mongodb" --template=mongodb-persistent  \
 -e MONGODB_USER=camel-k-example -e MONGODB_PASSWORD=transformations  \
 -e MONGODB_DATABASE=example -e MONGODB_ADMIN_PASSWORD=compleexpasswrd
